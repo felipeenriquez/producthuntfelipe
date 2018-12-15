94bc2905c7c4c706127c6c7980a505dee23d6997
felipeenriquez <felipe.enriquez@gmail.com>
2018-12-14 23:04:35 -0500

    crea metodo create

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,12 +10,11 @@ class ProductsController < ApplicationController
 	
 	def create
 		@product = Product.new(product_params)
-		if product.save
-			redirect_to products_path, notice: "El producto fue publicado con éxito"
+		if @product.save
+			redirect_to products_path
 		else
 			render :new
 		end
-		
 	end
 	
 	private
