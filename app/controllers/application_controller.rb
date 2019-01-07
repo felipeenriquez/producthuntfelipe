dd24ca8e6043b4ddb20f94022b198c69068e2652
felipeenriquez <felipe.enriquez@gmail.com>
2019-01-06 23:09:46 -0500

    restringe las paginas privadas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,12 @@ private
   end
   helper_method :current_user
   
+  def private_access
+    redirect_to :login unless signed_in?
+  end
+  
+  def public_acess
+    redirect_to root_path if signed_in?
+  end
+  
 end
